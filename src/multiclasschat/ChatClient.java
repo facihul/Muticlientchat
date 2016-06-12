4871c76924b05258ef8d3eb26fa192ae7a36e89f
panna <panna@localhost.localdomain>
2016-06-12 06:17:25 -0400

    Login window created and client server communication is working

--- src/multiclasschat/ChatClient.java
+++ src/multiclasschat/ChatClient.java
@@ -2,71 +2,98 @@ package multiclasschat;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.*;
 import java.net.*;
 
 import javax.swing.*;
 
-
 public class ChatClient extends JFrame implements Runnable {
-	
+
 	Socket socket;
 	JTextArea ta;
-	
-	int port =  3333;
+	JTextField tf;
+	JButton send;
+	JButton logout;
+	int port = 3330;
 	Thread thread;
-	
+
 	DataInputStream din;
 	DataOutputStream dout;
-	
+
 	String LoginName;
-	
-	ChatClient(String login) throws UnknownHostException, IOException{
+
+	ChatClient(String login) throws UnknownHostException, IOException {
 		super(login);
 		LoginName = login;
-	
+
 		ta = new JTextArea(18, 50);
-	     
+		tf = new JTextField(50);
+		send = new JButton("send");
+		logout = new JButton("logout");
+
+		send.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					dout.writeUTF(LoginName + "" + "Data" + tf.getText().toString());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+
+		});
+
+		logout.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					dout.writeUTF(LoginName + "" + "Data" + tf.getText().toString());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+
+		});
+
 		socket = new Socket("localhost", port);
-		
+
 		din = new DataInputStream(socket.getInputStream());
 		dout = new DataOutputStream(socket.getOutputStream());
-		
+
 		dout.writeUTF(LoginName);
 		dout.writeUTF(LoginName + " " + "LOGIN");
-		
+
 		thread = new Thread(this);
 		thread.start();
 		setup();
 	}
-	
-       public static void main(String[] args) throws IOException {
-       
-        ChatClient chatclient= new ChatClient("Client");
-        
-    }
-  
+
 	private void setup() {
 		// TODO Auto-generated method stub
 		setSize(600, 400);
-		
+
 		JPanel panel = new JPanel();
-		
+
 		panel.add(new JScrollPane(ta));
-		
+		panel.add(tf);
+		panel.add(send);
+		panel.add(logout);
+
 		add(panel);
-		
+
 		setVisible(true);
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		while(true){
+		while (true) {
 			try {
 				ta.append("\n" + din.readUTF());
 			} catch (IOException e) {
@@ -75,6 +102,9 @@ public class ChatClient extends JFrame implements Runnable {
 			}
 		}
 	}
-	
+
+
 
 }
+
+
--- src/multiclasschat/multiclientchat.java
+++ src/multiclasschat/multiclientchat.java
@@ -17,75 +17,106 @@ import java.util.logging.Logger;
  */
 public class multiclientchat {
 
-     static Vector  ClientSockets;
-     static Vector  LoginNames;
-     int port =  3333;
-     
-    public static void main(String[] args) throws IOException {
-       
-        multiclientchat server = new multiclientchat();
-        
-    }
-  
-    multiclientchat() throws IOException{
-     
-    ServerSocket server = new ServerSocket(port);
-    ClientSockets = new Vector();
-    LoginNames = new Vector();
-    
-    while (true){
-     Socket client = server.accept();
-     AcceptClient acceptclient = new AcceptClient(client); 
-    }
-    
-    
-    } 
+	static Vector ClientSockets;
+	static Vector LoginNames;
+	int port = 3333;
 
-     class AcceptClient extends Thread{
-        Socket ClientSocket;
-        DataInputStream DataIn;
-        DataOutputStream DataOut;
-        
-        
-        public AcceptClient(Socket client) throws IOException {
-            
-            ClientSocket = client;
-            DataIn = new DataInputStream(ClientSocket.getInputStream());
-            DataOut = new DataOutputStream(ClientSocket.getOutputStream());
-            
-            String LoginName= DataIn.readUTF();
-            LoginNames.add(LoginName);
-            ClientSockets.add(ClientSocket);
-            start();
-                 
-        }
-        
-       public void run(){
-        while(true){
-            try {
-                String msgClient = DataIn.readUTF();
-                StringTokenizer st =  new StringTokenizer(msgClient);
-                String LoginName = st.nextToken();
-                String msgType = st.nextToken();
-                
-               for(int i = 0; i<LoginNames.size(); i++){
-                 Socket psocket =(Socket)ClientSockets.elementAt(i);
-                 DataOutputStream pout = new DataOutputStream(psocket.getOutputStream());
-                 pout.writeUTF(LoginName + "has loged in !");
-               
-               } 
-                
-            } catch (IOException ex) {
-                Logger.getLogger(multiclientchat.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        
-        
-        }
-       
-       
-       } 
-        
-    }
-    
-    
+	public static void main(String[] args) throws IOException {
+
+		multiclientchat server = new multiclientchat();
+
+	}
+
+	multiclientchat() throws IOException {
+
+		ServerSocket server = new ServerSocket(port);
+		ClientSockets = new Vector();
+		LoginNames = new Vector();
+
+		while (true) {
+			Socket client = server.accept();
+			AcceptClient acceptclient = new AcceptClient(client);
+		}
+
+	}
+
+	class AcceptClient extends Thread {
+		Socket ClientSocket;
+		DataInputStream DataIn;
+		DataOutputStream DataOut;
+
+		public AcceptClient(Socket client) throws IOException {
+
+			ClientSocket = client;
+			DataIn = new DataInputStream(ClientSocket.getInputStream());
+			DataOut = new DataOutputStream(ClientSocket.getOutputStream());
+
+			String LoginName = DataIn.readUTF();
+			LoginNames.add(LoginName);
+			ClientSockets.add(ClientSocket);
+			start();
+
+		}
+
+		public void run() {
+			while (true) {
+				try {
+					String msgClient = DataIn.readUTF();
+					StringTokenizer st = new StringTokenizer(msgClient);
+					String LoginName = st.nextToken();
+					String msgType = st.nextToken();
+					String msg = "";
+					int io = -1;
+					
+					while(st.hasMoreTokens()){
+						msg = msg + ""+st.nextToken();
+					}
+					
+					if (msgType.equals("LOGIN")) {
+
+						for (int i = 0; i < LoginNames.size(); i++) {
+							Socket psocket = (Socket) ClientSockets.elementAt(i);
+							DataOutputStream pout = new DataOutputStream(psocket.getOutputStream());
+							pout.writeUTF(LoginName + "has loged in !");
+
+						}
+					}
+
+					else if (msgType.equals("LOGOUT")){
+
+						for (int i = 0; i < LoginNames.size(); i++) {
+							if(LoginNames == LoginNames.elementAt(i))
+								io = i;
+							Socket psocket = (Socket) ClientSockets.elementAt(i);
+							DataOutputStream pout = new DataOutputStream(psocket.getOutputStream());
+							pout.writeUTF(LoginName + "has loged out !");
+
+						}
+						
+						if (io >= 0){
+							LoginNames.removeElementAt(io);
+							ClientSockets.removeElementAt(io);
+							
+						}
+					}
+					else  {
+
+						for (int i = 0; i < LoginNames.size(); i++) {
+							Socket psocket = (Socket) ClientSockets.elementAt(i);
+							DataOutputStream pout = new DataOutputStream(psocket.getOutputStream());
+							pout.writeUTF(LoginName + ": " + msg);
+
+						}
+					}
+
+				} catch (IOException ex) {
+					Logger.getLogger(multiclientchat.class.getName()).log(Level.SEVERE, null, ex);
+				}
+
+			}
+
+		}
+
+	}
+
 }
